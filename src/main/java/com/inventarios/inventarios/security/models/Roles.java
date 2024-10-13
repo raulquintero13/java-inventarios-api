1881a51caa7f2fe0787bf425c4203ecbe46b665d
Raul Quintero <raul.quintero.cursoinfotec@gmail.com>
2024-10-13 12:40:34 -0700

    Tablas usuarios y roles

---
+++ src/main/java/com/inventarios/inventarios/security/models/Roles.java
@@ -0,0 +1,41 @@
+package com.inventarios.inventarios.security.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="role")
+public class Roles {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_role")
+	private Long idRole;
+	private String name;
+	
+	
+	
+	
+	public Roles() {
+		super();
+	}
+	
+	public Roles(Long idRole, String name) {
+		super();
+		this.idRole = idRole;
+		this.name = name;
+	}
+	
+	public Long getIdRole() {
+		return idRole;
+	}
+	public void setIdRole(Long idRole) {
+		this.idRole = idRole;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/main/java/com/inventarios/inventarios/security/models/Usuario.java
@@ -0,0 +1,77 @@
+package com.inventarios.inventarios.security.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name =  "id_usuario")
+	private Long idUsuario;
+	
+	private String username;
+	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(name = "usuarios_roles", joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id_usuario")
+	, inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id_role"))
+	private List<Roles> roles = new ArrayList<>();
+
+	
+	
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(Long idUsuario, String username, String password, List<Roles> roles) {
+		super();
+		this.idUsuario = idUsuario;
+		this.username = username;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Roles> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Roles> roles) {
+		this.roles = roles;
+	}
+	
+	
+	
+	
+	
+	
+
+}
